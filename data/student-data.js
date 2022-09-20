e5f4dcb70097d1d2fee951ff75a37df8e3d51149
AlyssaD <pink_lime@live.com>
2022-09-19 21:17:03 -0400

    initial commit

---
+++ data/student-data.js
@@ -0,0 +1,10 @@
+const students = [
+  {name: 'Robert', enrolled: true, gpa: 3.4},
+  {name: 'Sally', enrolled: false, gpa: 3.7},
+  {name: 'Eric', enrolled: false, gpa: 2.8},
+  {name: 'Clarissa', enrolled: true, gpa: 2.4}
+]
+
+export {
+  students
+}
